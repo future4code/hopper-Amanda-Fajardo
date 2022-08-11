f25dbc93342eb4f84e1ddba1b20b5926e0602e63
Amanda Viana Fajardo <amandaviana.jc@gmail.com>
2022-08-11 20:02:11 -0300

    aprofundamento typescript

---
+++ modulo-5/aprofundamento-typescript/src/exercicio-3.ts
@@ -0,0 +1,42 @@
+
+
+type post= {
+    autor: string,
+    texto: string
+  }
+  
+  const posts: post[] = [
+    {
+      autor: "Alvo Dumbledore",
+      texto: "Não vale a pena viver sonhando e se esquecer de viver"
+    },
+    {
+      autor: "Severo Snape",
+      texto: "Menos 10 pontos para Grifinória!"
+    },
+    {
+      autor: "Hermione Granger",
+      texto: "É levi-ô-sa, não levio-sá!"
+    },
+    {
+      autor: "Dobby",
+      texto: "Dobby é um elfo livre!"
+    },
+    {
+      autor: "Lord Voldemort",
+      texto: "Avada Kedavra!"
+    }
+  ]
+  
+  function buscarPostsPorAutor(posts: post[], autorInformado: string): post[] {
+    return posts.filter((post) => {
+        return post.autor === autorInformado
+      }
+    )
+  }
+  
+  console.log(buscarPostsPorAutor(posts,"Lord Voldemort"));
+  
+
+  //A entrada é por array e a saída é o post autor
+  
